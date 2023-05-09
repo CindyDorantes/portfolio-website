096dbea4782d1ed742cdb4cbc6304c6c5822a944
Cindy Dorantes <cindym.dorantes@gmail.com>
2023-05-08 21:52:04 -0600

    Add NavLinks to page sections

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,6 +1,6 @@
 import Link from 'next/link';
 import React from 'react';
-import { AiFillGithub, AiFillInstagram, AiFillLinkedin } from 'react-icons/ai';
+import { AiFillGithub, AiOutlineTwitter, AiFillLinkedin, AiOutlineMedium } from 'react-icons/ai';
 import { DiCssdeck } from 'react-icons/di';
 
 import { Container, Div1, Div2, Div3, NavLink, SocialIcons, Span } from './HeaderStyles';
@@ -9,11 +9,28 @@ const Header = () =>  (
   <Container>
     <Div1>
       <Link href="/">
-        <a style={{ display: "flex", alignItems: "center", color: "white"}}>
-          <DiCssdeck size="3rem" /> <Span>Portfolio</Span>
+        <a style={{ display: "flex", alignItems: "center", color: "white", marginBottom: "20px"}}>
+          <DiCssdeck size="3rem" /> <Span>Cindy Dorantes</Span>
         </a>
       </Link>
     </Div1>
+    <Div2>
+      <li>
+        <Link href="#projects">
+          <NavLink>Projects</NavLink>
+        </Link>
+      </li>
+      <li>
+        <Link href="#about">
+          <NavLink>About</NavLink>
+        </Link>
+      </li>
+      <li>
+        <Link href="#contact">
+          <NavLink>Contact</NavLink>
+        </Link>
+      </li>
+    </Div2>
   </Container>
 );
 
