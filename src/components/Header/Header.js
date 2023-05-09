5e1a83c8cd1ba4f2eaef1e988f9acc915ef12f86
Cindy Dorantes <cindym.dorantes@gmail.com>
2023-05-08 21:33:02 -0600

    Add Home link

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -3,12 +3,18 @@ import React from 'react';
 import { AiFillGithub, AiFillInstagram, AiFillLinkedin } from 'react-icons/ai';
 import { DiCssdeck } from 'react-icons/di';
 
-import { Container, Div1, Div2, Div3, NavLink, SocialIcons } from './HeaderStyles';
+import { Container, Div1, Div2, Div3, NavLink, SocialIcons, Span } from './HeaderStyles';
 
 const Header = () =>  (
-  <div>
-    Header
-  </div>
+  <Container>
+    <Div1>
+      <Link href="/">
+        <a style={{ display: "flex", alignItems: "center", color: "white"}}>
+          <DiCssdeck size="3rem" /> <Span>Portfolio</Span>
+        </a>
+      </Link>
+    </Div1>
+  </Container>
 );
 
 export default Header;
