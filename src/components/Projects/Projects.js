f94a3404843c48cce4182b2298617eb336b005dd
Cindy Dorantes <cindym.dorantes@gmail.com>
2023-05-10 12:50:03 -0600

    Add project section tag

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -5,9 +5,11 @@ import { Section, SectionDivider, SectionTitle } from '../../styles/GlobalCompon
 import { projects } from '../../constants/constants';
 
 const Projects = () => (
-  <div>
-    Projects
-  </div>
+  <Section nopadding id="projects">
+    <SectionDivider />
+    <SectionTitle main>My Recent Works</SectionTitle>
+    <GridContainer></GridContainer>
+  </Section>
 );
 
 export default Projects;
