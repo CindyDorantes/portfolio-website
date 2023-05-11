30899dcb44b521fc99afdd89bd4ca9ab6110c0d1
Cindy Dorantes <cindym.dorantes@gmail.com>
2023-05-10 22:29:05 -0600

    Add about section

--- src/components/Technologies/Technologies.js
+++ src/components/Technologies/Technologies.js
@@ -1,12 +1,20 @@
 import React from 'react';
-import { DiFirebase, DiReact, DiZend } from 'react-icons/di';
+import { DiFirebase, DiReact, DiGit } from 'react-icons/di';
 import { Section, SectionDivider, SectionText, SectionTitle } from '../../styles/GlobalComponents';
-import { List, ListContainer, ListItem, ListParagraph, ListTitle } from './TechnologiesStyles';
+import { List, ListContainer, ListItem, ListParagraph, ListTitle, RedButton } from './TechnologiesStyles';
 
 const Technologies = () =>  (
-  <div>
-    Technologies
-  </div>
+  <Section id="about">
+    <SectionDivider />
+    <SectionTitle><br />About me</SectionTitle>
+    <SectionText>
+    I can help you build a product, feature or website. Look through some of my work and experience! If you like what you see and have a project you need coded, don’t hestiate to contact me.
+    </SectionText>
+    <RedButton href="https://drive.google.com/file/d/1Ir5hmk7g8v1Q9MulCQYgfPPAf_yNT0MA/view?usp=share_link" style={{ width: "15rem"}}>
+      Get my resume
+    </RedButton>
+    
+  </Section>
 );
 
 export default Technologies;
