32a98f6081e4b3c5f60d6fe4d5599d3108fb5e3d
Cindy Dorantes <cindym.dorantes@gmail.com>
2023-12-22 05:38:40 -0600

    Modify the compiler setting in next config

--- next.config.js
+++ next.config.js
@@ -2,15 +2,8 @@
 const nextConfig = {
   /* config options here */
   compiler: {
-    // see https://styled-components.com/docs/tooling#babel-plugin for more info on the options.
-    styledComponents: true | {
-      // Enabled by default in development, disabled in production to reduce file size,
-      // setting this will override the default for all environments.
-      ssr: true,
-      // Enabled by default.
-    },
-  },
-  output: 'export',
+    styledComponents: true,
+  }
 }
  
 module.exports = nextConfig
